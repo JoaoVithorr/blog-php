85c526acd28311e1d84b1a387408a7ffe4e38c9a
João Vithor <joaovithormg@gmail.com>
2024-11-06 00:25:28 -0300

    Setup inicial do projeto

---
+++ helpers/url.php
@@ -0,0 +1,6 @@
+<?php
+
+$BASE_URL  = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/'; // definindo a url da página em uma variável 
+// isso ajuda no link do css ao arquivo index.php
+
+?>
