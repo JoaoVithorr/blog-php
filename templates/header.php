880c91199efd8967c0c551f32dd170a937ac94d4
João Vithor <joaovithormg@gmail.com>
2024-11-06 22:28:39 -0300

    header done

--- templates/header.php
+++ templates/header.php
@@ -15,5 +15,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    Template Cabeçalho
+    <a href="<?= $BASE_URL ?>">
+      <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Logo do site">
+    </a>
+    <nav>
+      <ul class="nav-bar">
+        <li><a href="<?= $BASE_URL ?>">Home</a></li>
+        <li><a href="#"></a>Categorias</li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="<?= $BASE_URL ?>/contact.php">Contato</a></li>
+      </ul>
+    </nav>
 </body>
